59742fe91626d2ce447c823d57369a3d15b988ed
Hayes Choy <hayeselnut2127@gmail.com>
2020-08-23 23:34:44 +1000

    got bg, got most AWS APIs working, now need to actually write the code for calculations

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
-// var RIOT_API_KEY;
-// var ref = firebase.database().ref("dewoh");
-// var summonerIdsRef = ref.child("summonerIds");
+"use strict";
 
-var latestVersion;
+let latestVersion;
+
+const WIN = 1;
+const LOSE = 0;
+const VS = -1;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+function getIntersection(arr1, arr2) {
+    return arr1.filter(value => arr2.includes(value));
+}
+
+function showStatus(statusMessage) {
+    const duration = 1000;
+    const style = "h2"
+
+    $("#status").fadeOut(duration, () => {
+        $("#status").html(`<${style}>${statusMessage}</${style}>`).fadeIn(duration);
+    });
+}
+
+
 
 function getLatestVersion() {
     $.ajax({
@@ -18,52 +39,53 @@ function getLatestVersion() {
 }
 
 $(document).ready(() => {
-    // ref.on("value", snapshot => {
-    //     RIOT_API_KEY = snapshot.val().RIOT_API_KEY;
-    // }, error => {
-    //     alert("ERROR: " + error.code);
-    // });
-
     getLatestVersion();
-
 });
-
-function checkSummonerIdInFirebase(region, name) {
-
-    var regRef = summonerIdsRef.child(region);
-
-    return regRef.once("value").then(snapshot => {
-        if (snapshot.hasChild(name)) {
-            return snapshot.child(name).val();
-        }
-
-        return null;
-    });
-}
 
 function getSummonerDTO(region, name) {
     return $.ajax({
         type: "GET",
         url: `https://dkwuj1k34l.execute-api.us-east-2.amazonaws.com/rgapi/summoner/${region}/${name}`,
         success: response => {
-            console.log(response);
             return response;
         }, error: err => {
-            console.log("err", err.status);
+            console.log("err get summoner", err.status);
             // TODO: SHOW INVALID SUMMONER NAME -- "name" not found
         }
     });
 }
 
-async function addSummonerIdToFirebase(region, name, id) {
+function getMatchlistDTO(region, accountId, index) {
+    return $.ajax({
+        type: "GET",
+        url: `https://dkwuj1k34l.execute-api.us-east-2.amazonaws.com/rgapi/matchlist/${region}/${accountId}/${index}`,
+        success: response => {
+            return response;
+        }, error: err => {
+            console.log("err get matchlist", err);
+            // TODO: when matchlist hit error (maybe over index?)
+        }
+    });
+}
 
-    summonerIdsRef.child(region).child(name).set(id);
+function getMatchDTO(region, gameId) {
+    return $.ajax({
+        type: "GET",
+        url: `https://dkwuj1k34l.execute-api.us-east-2.amazonaws.com/rgapi/match/${region}/${gameId}`,
+        success: response => {
+            console.log("game info found, ", response);
+            return response;
+        }, error: err => {
+            console.log("err get match", err);
+            // TODO: HANDLE THIS (e.g. invalid gameiD? is that even possible)
+        }
+    });
 }
 
 function showSummonerMetadata(summonerDTO) {
-    name = summonerDTO.name;
-    summonerIcon = summonerDTO.profileIconId;
-    level = summonerDTO.summonerLevel;
+    const name = summonerDTO.name;
+    const summonerIcon = summonerDTO.profileIconId;
+    const level = summonerDTO.summonerLevel;
 
     $("#dewoh-results").css("display", "block");
     $("#summoner-metadata").append(`<p>${name}, Level ${level} - icon: ${summonerIcon}</p>`);
@@ -72,33 +94,62 @@ function showSummonerMetadata(summonerDTO) {
 
 async function getSummonerId(region, name) {
 
-    // var id = await checkSummonerIdInFirebase(region, name);
-
-    // if (id) {
-    //     return id;
-    // }
-
-    // // Not in firebase
-
     // Call API to find ID
-    summonerDTO = await getSummonerDTO(region, name);
-
-    showSummonerMetadata(summonerDTO);
+    const summonerDTO = await getSummonerDTO(region, name);
 
     if (!summonerDTO) {
         console.log("summonerDTO dtected as null");
         return null;
     }
 
-    id = summonerDTO.accountId;
-    console.log("summonerDTO detected", id);
-
-    // Add to database
-    // addSummonerIdToFirebase(region, name, id);
+    // Show summoner information
+    showSummonerMetadata(summonerDTO);
 
     // Return id
-    return id;
+    return summonerDTO.accountId;
 
+}
+
+async function getGameIds(region, accountId, timestamp) {
+    let idx = 0;
+    let matchlistDTO = await getMatchlistDTO(region, accountId, idx);
+    let gameIds = matchlistDTO.matches.filter(m => m.timestamp > timestamp).map(m => m.gameId);
+
+    let keepLooking = gameIds.length == 100;
+    while (keepLooking) {
+        idx += 100;
+
+        matchlistDTO = await getMatchlistDTO(region, accountId, idx);
+        let moreGameIds = matchlistDTO.matches.filter(m => m.timestamp > timestamp).map(m => m.gameId);
+        keepLooking = moreGameIds.length == 100;
+
+        gameIds = gameIds.concat(moreGameIds);
+
+        //TODO: handle case where you hit the end of someones match history
+    }
+
+    return gameIds;
+}
+
+function isSameTeam(matchDTO, id1, id2) {
+
+}
+
+async function getGameOutcomes(commonGames, region, id1, id2) {
+    // WHEN I GET BETTER API LIMITS: const commonMatchDTOs = await Promise.all(commonGames.map(m => getMatchDTO(region, m)));
+
+    const gameResults = {
+        "win": 0,
+        "loss": 0
+    };
+
+    commonGames.foreach(gameId => {
+        const matchDTO = await getMatchDTO(region, gameId);
+
+        
+
+        await sleep(1000);
+    });
 }
 
 $("input").on("focus", function() {
@@ -106,16 +157,14 @@ $("input").on("focus", function() {
 });
 
 function transformSummonerName(name) {
-    name = name.toLowerCase();
-    name = name.split(" ").join("");
-
-    return name;
+    return name.toLowerCase().split(" ").join("");
 }
 
+
 $("#dewoh-btn").on("click", async () => {
-    var region = $("#region").val();
-    var name1 = transformSummonerName($("#sum1-txtbox").val());
-    var name2 = transformSummonerName($("#sum2-txtbox").val());
+    const region = $("#region").val();
+    const name1 = transformSummonerName($("#sum1-txtbox").val());
+    const name2 = transformSummonerName($("#sum2-txtbox").val());
 
     if (name1 === "") {
         console.log("name1 is blank");
@@ -129,9 +178,22 @@ $("#dewoh-btn").on("click", async () => {
 
     $("#summoner-metadata").empty();
 
-    var id1 = await getSummonerId(region, name1);
-    var id2 = await getSummonerId(region, name2);
+    const id1 = await getSummonerId(region, name1);
+    const id2 = await getSummonerId(region, name2);
 
+    // Promise.all([id1, id2]); // Async for both ids
 
-    // console.log(sum1, sum2);
+    let timestamp = 1577836800 * 1000;
+
+    showStatus("Loading match histories...");
+
+    const games1 = await getGameIds(region, id1, timestamp);
+    const games2 = await getGameIds(region, id2, timestamp);
+
+    const commonGames = getIntersection(games1, games2);
+    showStatus(`Checking ${commonGames.length} common games...`);
+
+    console.log("COMMON GAMES", commonGames);
+
+    getGameOutcomes(commonGames, region, id1, id2);
 });
