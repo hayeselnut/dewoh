af7d3839d2d81c19ffde18dea6f4322f862830d7
Hayes Choy <hayeselnut2127@gmail.com>
2022-07-10 01:39:02 +1000

    hover shows close button

--- src/components/summoner-card.tsx
+++ src/components/summoner-card.tsx
@@ -1,8 +1,11 @@
-import React, { FC } from 'react';
-import { Box, Image, Input } from '@chakra-ui/react';
+import React, { FC, useState } from 'react';
+import { Box, Image, Input, IconButton } from '@chakra-ui/react';
+import { CloseIcon } from '@chakra-ui/icons';
 import HelmetBro from '../assets/helmet-bro.svg';
 
 const SummonerCard: FC<{ value: string, onChange: (val: string) => void }> = ({ value, onChange }) => {
+  const [showCloseButton, setShowCloseButton] = useState(false);
+
   return (
     <Box
       display='flex'
@@ -11,11 +14,25 @@ const SummonerCard: FC<{ value: string, onChange: (val: string) => void }> = ({ 
       alignItems='center'
       borderWidth='2px'
       borderRadius='lg'
+      position='relative'
       maxW='12%'
       bg='white'
       p='2'
       m='2'
+      onMouseEnter={() => setShowCloseButton(true)}
+      onMouseLeave={() => setShowCloseButton(false)}
     >
+      <IconButton
+        icon={<CloseIcon />}
+        aria-label='Delete summoner'
+        bg='black'
+        borderRadius='full'
+        position='absolute'
+        size='xs'
+        top='-3'
+        right='-3'
+        visibility={showCloseButton ? 'visible' : 'hidden'}
+      />
       <Box borderRadius='full' bg='gray.200' p='12' m='2'>
         <Image
           boxSize='12'
