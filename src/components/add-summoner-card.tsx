8bb16d4107c3d71176a5056a276fd1c83240e0f6
Hayes Choy <hayeselnut2127@gmail.com>
2022-07-14 23:11:11 +1000

    add summoner icon button is tabbable

--- src/components/add-summoner-card.tsx
+++ src/components/add-summoner-card.tsx
@@ -4,21 +4,23 @@ import { AddIcon } from '@chakra-ui/icons';
 
 const AddSummonerCard: FC<{onClick: () => void}>= ({ onClick }) => {
   return (
-    <Box
-      borderRadius='full'
-      borderStyle='dashed'
-      borderWidth='2px'
-      p='10'
-      m='4'
-      opacity='0.2'
-      _hover={{
-        opacity: 1,
-        cursor: 'pointer',
-      }}
-      onClick={() => onClick()}
-    >
-      <AddIcon w={6} h={6} color='white'/>
-    </Box>
+    <button onClick={() => onClick()}>
+      <Box
+        borderRadius='full'
+        borderStyle='dashed'
+        borderWidth='2px'
+        p='10'
+        m='4'
+        opacity='0.2'
+        _hover={{
+          opacity: 1,
+          cursor: 'pointer',
+        }}
+      >
+        <AddIcon w={6} h={6} color='white'/>
+      </Box>
+    </button>
+
   );
 };
 
